f4ab9784d44ce47289c86ea11373069995d0ce08
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2024-07-01 15:19:39 +0530

    add Nothin Present tag

--- src/components/BookingList.js
+++ src/components/BookingList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import BookingItem from './BookingItem';
 
 const BookingList = ({ bookings, deleteBooking, editBooking }) => {
+   if(bookings.length === 0){
+    return <h1>Nothing Present</h1>
+   }
   return (
     <ul>
       {bookings.map(booking => (
